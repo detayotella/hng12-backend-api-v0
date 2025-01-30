14d189b89ca58cd0383c7e4f937847357d3bd963
detayotella <tellaadetayo@gmail.com>
2025-01-30 14:30:37 +0100

    update the controllers and routes

--- src/routes/infoRoutes.js
+++ src/routes/infoRoutes.js
@@ -3,6 +3,6 @@ import express from "express";
 
 const router = express.Router(); 
 
-router.get("/info", getParticipantsInfo); 
+router.get("/", getParticipantsInfo); 
 
 export default router; 
--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(cors()); 
 
-app.use("/api", infoRoutes); 
+app.use(infoRoutes); 
 
 app.listen(PORT, () => {
     console.log(`Server is running at port: ${PORT}`); 
